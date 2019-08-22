45a837650bb1cc77263d0e7b68b1c81c9d9de44b
RahebahAli <371202993@qu.edu.sa>
2019-08-22 11:35:23 -0800

    Create Day3

---
+++ Day3.py
@@ -0,0 +1,14 @@
+FirstName = "Rahebah "
+LastName = "Ali"
+
+print (FirstName + LastName) #will be display full name : (Rahebah Ali)
+
+year = 2019
+print (year) #will be display the year as a number
+
+year = "Two thousand and nineteen"
+print (year) #After assign another value : will be display the year as a string
+
+age = 20
+x ="Years"
+print(age + x) #Error : becaus can not combine a string and a number مبروك أول Error
