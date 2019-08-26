4d7089c80627cf359289e6acb3b5353c633da26a
RahebahAli <371202993@qu.edu.sa>
2019-08-26 13:27:50 -0800

    Strings

---
+++ Day7_Strings.py
@@ -0,0 +1,13 @@
+#Single quotation marks is the same as double quotation marks.
+print ("Hello, world")
+print ('Hello, world')
+
+#Assign String to a Variable
+a = "Hello, world"
+print (a)
+print(a[7:12]) #Strings are Arrays
+
+#Multiline Strings
+b = '''Optimism is the faith that leads to achievement.
+Nothing can be done without hope and confidence.'''
+print(b[49:97]) #Strings are Arrays
