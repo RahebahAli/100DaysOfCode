c7efd7dfb7baf81c7a7cb902ad7dc32fe834db39
RahebahAli <371202993@qu.edu.sa>
2019-08-27 08:00:57 -0800

    String2

---
+++ Day8_Strings2.py
@@ -0,0 +1,18 @@
+a = " Don't let the past hold you back , you're missing the good stuff "
+
+print(len(a)) #66
+
+print(a.strip()) 
+#Don't let the past hold you back , you're missing the good stuff
+
+print(a.lower()) 
+# don't let the past hold you back , you're missing the good stuff
+
+print(a.upper()) 
+# DON'T LET THE PAST HOLD YOU BACK , YOU'RE MISSING THE GOOD STUFF
+
+print(a.split(",")) 
+#[" Don't let the past hold you back ", " you're missing the good stuff "]
+
+print(a.replace("good", "beautiful")) 
+# Don't let the past hold you back , you're missing the beautiful stuff
