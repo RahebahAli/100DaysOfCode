9556852dfca93baaee92622e09f34642e0150f61
RahebahAli <371202993@qu.edu.sa>
2019-08-30 17:59:11 -0800

    Operators

---
+++ Day10_Operators.py
@@ -0,0 +1,32 @@
+a = 5
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a ** b) #Power
+print(a / b)
+print(a // b) #Division without fractions
+print(a % b)
+
+
+x = 6
+print(x)
+x |= 3 # Or : in binary number
+print(x)
+x &= 2 # And : in binary number
+print(x)
+x ^= 4 # Xor : in binary number
+print(x)
+x <<= 5 # Left shift  in binary number, same as : x * (2 ** 5)
+print(x)
+x >>= 6 # Right shift  in binary number, same as : x / (2 ** 5)
+print(x)
+
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a <= b)
