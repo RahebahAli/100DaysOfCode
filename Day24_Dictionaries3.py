fca10e71eb24172e587a102f263bf51c0a319e02
RahebahAli <371202993@qu.edu.sa>
2019-09-14 13:48:58 -0800

    Dictionaries3

---
+++ Day24_Dictionaries3.py
@@ -0,0 +1,10 @@
+Student1 = {'ID': 1102, 'Name': 'Ali', 'Level': 'Six', 'Age': 20}
+Student2 = {'ID': 1523, 'Name': 'Saad', 'Level': 'Six', 'Age': 21}
+
+Student3 = Student1.copy()
+print(Student3)
+
+Student4 = dict(ID=3567, Name="Ahmad", Level="Six")
+
+Students = {"Student1":Student1, "Student2":Student2, "Student4":Student4 }
+print(Students)
