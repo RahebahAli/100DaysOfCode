7073941d677b2887414b01cb2531bdd985e01c4e
RahebahAli <371202993@qu.edu.sa>
2019-10-04 01:46:45 -0800

    Lambda

---
+++ Day35_Lambda.py
@@ -0,0 +1,18 @@
+#Squared numbers
+seq = lambda number : number * number
+print (seq(3))
+print (seq(4))
+print (seq(7))
+print (seq(9))
+
+#------------------------------------
+
+Hi = lambda name : print("Hello, " + name)
+name = str (input("Enter Your Name : "))
+Hi(name)
+
+#------------------------------------
+
+#Convert to a float
+floatNumber = lambda number : float(number)
+print(floatNumber(4)) 
