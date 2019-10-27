edcc44476e64e5ce47bdea689c8ee5524f13b802
RahebahAli <371202993@qu.edu.sa>
2019-10-27 10:17:40 -0800

    PIP

---
+++ Day62_PIP.py
@@ -0,0 +1,10 @@
+# pip install camelcase (in cmd) >>to install the Package
+import camelcase
+
+text = "Be confident in the power of God and you will never regret."
+c = camelcase.CamelCase().hump(text)
+print(c)
+
+# pip uninstall camelcase (in cmd) >>to uninstall the Package
+
+#pip list (in cmd) >> to list all the packages installed on your system:
